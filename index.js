d74b1753ec4085507ecf44082a54192defa2af33
rbcatong <rbcatong@gmail.com>
2020-11-12 21:45:21 -0500

    writing loop for affirmation

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function sendOutAffirmation(){
     document.getElementById('linesOfAffirmation').style.display = 'block';
-}
+    let mixOfAffirmations = ['You are pretty', 'You can do it!']
+    for(i=0; i < mixOfAffirmations.length; i++){
+        //show each element 
+    }
+}   
