3fed7a63384ed852c58297fd4d2ed76311fc0c53
rbcatong <rbcatong@gmail.com>
2020-11-14 17:34:15 -0500

    able to print out affirmation

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 function sendOutAffirmation(){
     document.getElementById('linesOfAffirmation').style.display = 'block';
+   
     let mixOfAffirmations = ['You are pretty', 'You can do it!']
     for(i=0; i < mixOfAffirmations.length; i++){
         //show each element 
+        document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[i]
+        console.log(mixOfAffirmations[i])
     }
 }   
