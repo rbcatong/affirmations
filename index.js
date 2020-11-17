119142c49df99990976a9d1f82235327f4bb094b
rbcatong <rbcatong@gmail.com>
2020-11-17 16:59:16 -0500

    figuring out why the array isnt working

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ function sendOutAffirmation(){
     document.getElementById('linesOfAffirmation').style.display = 'block';
    
     let mixOfAffirmations = ['You are pretty', 'You can do it!','Great job']
-    for(i=0; i < mixOfAffirmations.length; i++){
+
+    for(let i=0; i < mixOfAffirmations.length; i++){
         //show each element 
+        console.log(mixOfAffirmations[i])
         document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[i]
+        document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[i]
+        document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[i]
+
+        // consoles all 3 at the same time ... 
         //loop AND KEEP Pushing out the lines 
         
     }
