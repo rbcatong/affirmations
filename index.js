c384b56353d4fb9b8781c49e7107086edecf293f
rbcatong <rbcatong@gmail.com>
2020-11-17 16:51:10 -0500

    added a gif

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function sendOutAffirmation(){
     document.getElementById('linesOfAffirmation').style.display = 'block';
    
-    let mixOfAffirmations = ['You are pretty', 'You can do it!']
+    let mixOfAffirmations = ['You are pretty', 'You can do it!','Great job']
     for(i=0; i < mixOfAffirmations.length; i++){
         //show each element 
         document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[i]
-        console.log(mixOfAffirmations[i])
+        //loop AND KEEP Pushing out the lines 
+        
     }
 }   
