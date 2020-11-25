fba4a0b9abff8dcc55a41dbef977ad35c469dd5a
rbcatong <rbcatong@gmail.com>
2020-11-25 13:55:19 -0500

    changing the design of date

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function(){
 function sendOutAffirmation(){
     document.getElementById('linesOfAffirmation').style.display = 'block';
    
-    let mixOfAffirmations = ['You are pretty', 'You can do it!','Great job']
+    let mixOfAffirmations = ['You are pretty', 'You can do it!','Great job', 'You are amazing'] 
 
     let randomIndex = Math.floor(Math.random() * mixOfAffirmations.length);
     document.getElementById('linesOfAffirmation').innerHTML = mixOfAffirmations[randomIndex]
